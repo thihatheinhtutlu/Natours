52dcce73e672dd0f26d65665ffa6e3443d5c0545
thihatheinhtutlu <linae0343@gmail.com>
2024-01-30 16:00:06 +0630

    updated image compositions with cool animation effect

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,11 @@
 import './home.scss';
 import Button from '../button/button';
 
+
+import nat1 from '../../../src/asset/img/nat-1-large.jpg'
+import nat2 from '../../../src/asset/img/nat-2-large.jpg'
+import nat3 from '../../../src/asset/img/nat-3-large.jpg'
+
 function Home() {
     return (
         <main>
@@ -26,7 +31,13 @@ function Home() {
                     <Button buttonType='text'>Learn more &rarr;</Button>
                 </div>
                 <div className='col-1-of-2'>
-                    Image composition
+                    <div className="composition">
+                    
+                        <img src={nat1} alt="Photo 1" className="composition__photo composition__photo--p1"/>
+                        <img src={nat2} alt="Photo 2" className="composition__photo composition__photo--p2"/>
+                        <img src={nat3} alt="Photo 3" className="composition__photo composition__photo--p3"/>
+
+                    </div>
                 </div>
             </div>
 
